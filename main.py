2a3ea8a0ace0e52ce536a1b481bd88f2e27228c8
Aleksandr Zavialov <madnoobka@yandex.ru>
2023-04-09 23:13:29 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_lady(name, married):
+    if married:
+        print(f'Hello, Mrs {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    else:
+        print(f'Hi, Ms {name}')
+
+
+print_lady('Sarah', False)
